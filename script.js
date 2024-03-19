c7b17810f7181ce650da29585a6acbae683aed8b
Lauren Elisiario <lauren.elisiario@gmail.com>
2024-03-19 00:54:08 -0700

    Added date stamp. Need to figure out autoupdating time from Unix time.

--- script.js
+++ script.js
@@ -13,14 +13,23 @@ $(function () {
   // useful when saving the description in local storage?
 
 
-
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
 
+  // Display the current date in the header
+  $('#currentDay').text(dayjs().format('dddd, MMMM D'));
 
+  // Apply initial class based on current time
+  updateTimeBlocks();
+
+  // Display any saved descriptions
+  displaySavedDescriptions();
+
+  // Update time blocks every minute
+  setInterval(updateTimeBlocks, 60000);
 
 
   // TODO: Add code to get any user input that was saved in localStorage and set
