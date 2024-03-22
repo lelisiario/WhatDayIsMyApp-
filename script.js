4fdcd9fd94d3268f0246b596fd0b0a832748ea5c
Lauren Elisiario <lauren.elisiario@gmail.com>
2024-03-22 09:36:45 -0700

    Added save functionality.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 // in the html.
 
 $(function () {
+  
   $('.saveBtn').on('click', function () {
     // Get the description from the corresponding textarea
     var description = $(this).siblings('.description').val();
@@ -12,7 +13,20 @@ $(function () {
     localStorage.setItem(hourId, description);
   });
 
-  
+    // Function to apply past, present, or future class to each time block
+    function updateTimeBlocks() {
+      var currentHour = dayjs().hour();
+      $('.time-block').each(function () {
+        var hour = parseInt($(this).attr('id').split('-')[1]);
+        if (hour < currentHour) {
+          $(this).removeClass('present future').addClass('past');
+        } else if (hour === currentHour) {
+          $(this).removeClass('past future').addClass('present');
+        } else {
+          $(this).removeClass('past present').addClass('future');
+        }
+      });
+    }
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -20,7 +34,14 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-
+  // Function to get user input saved in localStorage and set textarea values
+  function displaySavedDescriptions() {
+    $('.time-block').each(function () {
+      var hourId = $(this).attr('id');
+      var description = localStorage.getItem(hourId);
+      $(this).find('.description').val(description);
+    });
+  }
 
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
